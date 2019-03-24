10f7b3d3d6b967265487bfbd6e45bd129e2a9cc6
dudxo32 <37172704+dudxo32@users.noreply.github.com>
2019-03-24 15:59:02 +0900

    채널리스트 레이아웃

--- app/src/main/java/com/example/capstone/LoginActivity.java
+++ app/src/main/java/com/example/capstone/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,6 +16,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         editID = (EditText)findViewById(R.id.edit_id);
         editPW = (EditText)findViewById(R.id.edit_pw);
+
     }
     public void LoginClick(View v)
     {
@@ -22,5 +24,12 @@ public class LoginActivity extends AppCompatActivity {
         //값보내기
         //       goList.putExtra("key", editID.getText().toString());
         startActivity(goList);
+    }
+    public void JoinusClick(View v)
+    {
+        Intent goJoin = new Intent(LoginActivity.this, JoinActivity.class);
+        //값보내기
+        //       goList.putExtra("key", editID.getText().toString());
+        startActivity(goJoin);
     }
 }
